e4512e6660f040ca4aefd8b5fdee90d889a2b49f
anyuruf <anyuruf@gmail.com>
2023-02-11 13:08:51 +0300

    Added logging and gateway route obj

--- gateway-service/src/main/java/net/anyuruf/geneology/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/anyuruf/geneology/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,20 @@ package net.anyuruf.geneology.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes().route("member-basic", r -> r.path("/**").uri("lb://fedora:8020")).build();
 	}
 
 }
